4f4c04024622c4924e7b707b462c713d0321be6e
rtsarakaki <rtsarakaki@gmail.com>
2023-09-23 16:27:08 -0300

    export validations and valueobjects

---
+++ src/ValueObjects/index.ts
@@ -0,0 +1,6 @@
+export { FullName } from './Person/FullName.type';
+
+export { LongDescription } from './StringLiteral/LongDescription.type'
+export { ShortDescription } from './StringLiteral/ShortDescription.type'
+export { SingleWord } from './StringLiteral/SingleWord.type'
+export { Title } from './StringLiteral/Title.type'
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import messages from './Resources/Resources.json'
-
-console.log(messages.validations[0].messages[0])
-
-
-
-
-
-console.log("A Typescript library of classes aimed to help developers using and undestanding immutable objects")
