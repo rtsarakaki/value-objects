7615871151034c329061145b48b156a31d26ff12
rtsarakaki <rtsarakaki@gmail.com>
2023-09-24 10:33:15 -0300

    Add export to createEntities functions

--- src/ValueObjects/index.ts
+++ src/ValueObjects/index.ts
@@ -1,6 +1,14 @@
 export { FullName } from './Person/FullName.type';
+export { createFullName } from './Person/FullName.type';
 
 export { LongDescription } from './StringLiteral/LongDescription.type'
+export { createLongDescription } from './StringLiteral/LongDescription.type'
 export { ShortDescription } from './StringLiteral/ShortDescription.type'
+export { createShortDescription } from './StringLiteral/ShortDescription.type'
 export { SingleWord } from './StringLiteral/SingleWord.type'
+export { createSingleWord } from './StringLiteral/SingleWord.type'
 export { Title } from './StringLiteral/Title.type'
+export { createTitle } from './StringLiteral/Title.type'
+
+export { UUID } from './UUID/UUID.type'
+export { createUUID } from './UUID/UUID.type'
